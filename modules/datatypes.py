9c9d8c347b385fd60fccdc8fc494be7cb1f9b479
wachinaba <wachinaba@gmail.com>
2021-04-01 02:11:47 +0900

    [update] rename to datetypes.py

--- modules/datatypes.py
+++ modules/datatypes.py
@@ -9,26 +9,35 @@ class Childs():
       self.data = data
       self.prev = None
       self.next = None
-      self._len = 0
 
   def __init__(self, *iterable):
     self.head = self.head.prev = self.head.next = Childs.Node()
+    self._len = 0
+
+  def __len__(self):
+    return self._len
 
   def append(self, data=None):
-    prev_node = self.head
-    next_node = self.next
     new_node = Childs.Node(data)
-
-    prev_node.next = next_node.prev = new_node
-    new_node.prev = prev_node
-    new_node.next = next_node
-
+    new_node.prev = self.head
+    new_node.next = self.head.next
+    self.head.next = self.head.next.prev = new_node
     self._len += 1
+    return new_node
 
   def move_top(self, node):
-    
-      
+    node.next.prev = node.prev.next
+    node.prev = self.head
+    node.next = self.head.next
+    self.head.next = self.head.next.prev = node
 
+  def printall(self):
+    n = self.head.next
+    while not n == self.head:
+      print(n.data)
+      n = n.next
+      
+"""
   def __iter__(self):
     return ChildsIterator(self)
 
@@ -42,3 +51,4 @@ class ChildsIterator():
 
   def __next__(self):
     pass
+"""
