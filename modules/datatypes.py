8672c845e0bdb448db6205e7f23dc7152b8c8d42
wachinaba <wachinaba@gmail.com>
2021-04-05 11:02:53 +0900

    [sync]

--- modules/datatypes.py
+++ modules/datatypes.py
@@ -7,8 +7,8 @@ class Rect():
 
 class Childs():
   class Node():
-    def __init__(self, data=None):
-      self.data = data
+    def __init__(self, container=None):
+      self.container = container
       self._prev = None
       self._next = None
 
@@ -21,14 +21,19 @@ class Childs():
   def __len__(self):
     return self._len
 
-  def append(self, data=None):
-    new_node = Childs.Node(data)
+  def append(self, container=None):
+    new_node = Childs.Node(container)
     new_node._prev = self._head
     new_node._next = self._head._next
     self._head._next._prev = new_node
     self._head._next = new_node
     self._len += 1
     return new_node
+
+  def delete(self, node):
+    node._prev._next = node._next
+    node._next._prev = node._prev
+    self._len -= 1
 
   def move_top(self, node):
     node._prev._next = node._next
@@ -42,7 +47,7 @@ class Childs():
     n = self._head._next
     i=0
     while not n == self._head:
-      print(n.data)
+      print(n.container)
       n = n._next
       i += 1
       if i == self._len : break
--- modules/gui.py
+++ modules/gui.py
@@ -1,12 +1,26 @@
 import datatypes as dt
 
 class BaseContainer():
+
+  STATE_STAY = 0
+  STATE_NEEDREDRAW = 1
+
   def __init__(self, rect: dt.Rect):
     self.rect = rect
     self.childs = dt.Childs()
+    self.state = 
+    self.parent = None
 
-  def append(self, child):
-    self.childs.append(child)
+  def send_update
+
+  def append(self, container):
+    container.parent = self
+    return self.childs.append(container)
+
+  def delete(self, node):
+    return self.childs.delete(node)
+
+  
 
   
 
