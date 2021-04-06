b4b12cafee59ef1a583e1b87af3feb155dd27cd2
wachinaba <wachinaba@gmail.com>
2021-04-06 11:10:15 +0900

    [update] class member variable

--- modules/gui.py
+++ modules/gui.py
@@ -5,22 +5,22 @@ class BaseContainer():
   STATE_STAY = 0
   STATE_NEEDREDRAW = 1
 
-  def __init__(self, rect: dt.Rect):
+  def __init__(self, rect: dt.Rect, parent: BaseContainer):
     self.rect = rect
     self.childs = dt.Childs()
-    self.state = 
-    self.parent = None
+    self.state = self.STATE_STAY
+    self.parent = parent
+    self.node = None
 
-  def send_update
+  def send_redraw(self):
+    self.state = self.STATE_NEEDREDRAW
+    if not self.parent:
+      self.parent.send_redraw()
 
   def append(self, container):
     container.parent = self
-    return self.childs.append(container)
+    appended_node = self.childs.append(container)
+    appended_node.container.node = appended_node
 
-  def delete(self, node):
-    return self.childs.delete(node)
-
-  
-
-  
-
+  def detach(self):
+    return self.childs.delete(self.node)
